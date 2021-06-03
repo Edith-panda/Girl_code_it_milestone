68b391c1c90388331d088cec4d6f3f120d4ba84f
yashasvi sakure <yashasvisakure@gmail.com>
2021-06-03 20:33:28 +0530

    Milestone 1

---
+++ +-0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    if (n == 0){
+        cout << "NO is ZERO";
+
+    }
+    else if ( n <0){
+        cout << "negative no";
+    }
+    else {
+        cout << " Positive";
+    }
+    return 0;
+} 
